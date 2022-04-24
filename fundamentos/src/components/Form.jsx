f0f4c55ac7816bde3bdf1eb2308f43c86b342bef
hiceklent <hiceklent@gmail.com>
2022-04-24 02:48:50 -0500

    Aprendiendo React v2 - ELIMINAR AÑADIDO

--- fundamentos/src/components/Form.jsx
+++ fundamentos/src/components/Form.jsx
@@ -1,9 +1,11 @@
+import { nanoid } from "nanoid";
 import React from "react";
 
 const Form = () => {
   const [fruta, setFruta] = React.useState("");
   const [descripcion, setDescripcion] = React.useState("");
   const [listafrutas, setListaFrutas] = React.useState([]);
+  const [id, setId] = React.useState ('');
 
   const guardarFrutas = (e) => {
     e.preventDefault();
@@ -15,11 +17,13 @@ const Form = () => {
 
     if (!descripcion.trim()) {
       alert("Digite la descripcion");
+      return
     }
 
     setListaFrutas([
       ...listafrutas,
       {
+        id: nanoid(),
         nombreFruta: fruta,
         nombreDescripcion: descripcion,
       },
@@ -30,6 +34,16 @@ const Form = () => {
     setDescripcion("");
   };
 
+const editar = item => {
+  setFruta(item.nombreFruta);
+  setDescripcion(item.nombreDescripcion);
+}
+
+const eliminaritem = id => {
+  const aux = listafrutas.filter(item => item.id !== id)
+  setListaFrutas(aux)
+};
+
   return (
     <div>
       <h1 className="text-center">CRUB</h1>
@@ -39,15 +53,15 @@ const Form = () => {
           <h4 className="text-center">Listado de frutas</h4>
           <br />
           <ul className="list-group">
-            {listafrutas.map((item, index) => (
-              <li className="list-group-item" key={index}>
+            {listafrutas.map((item) => (
+              <li className="list-group-item" key={item.id}>
                 <spam className="lead">
                   {item.nombreFruta} <br /> {item.nombreDescripcion}
                 </spam>
-                <button className="btn btn-danger btn-sm float-end mx-2">
+                <button className="btn btn-danger btn-sm float-end mx-2" onClick={ ()=> eliminaritem(item.id) }>
                   Eliminar
                 </button>
-                <button className="btn btn-warning btn-sm float-end">
+                <button className="btn btn-warning btn-sm float-end" onClick={()=>editar(item)}>
                   Editar
                 </button>
               </li>
@@ -62,12 +76,14 @@ const Form = () => {
               type="text"
               placeholder="Ingrese Su Fruta"
               onChange={(e) => setFruta(e.target.value)}
+              value={fruta}
             />
             <input
               className="form-control mb-2"
               type="text"
               placeholder="Ingrese Descripcion"
               onChange={(e) => setDescripcion(e.target.value)}
+              value={ descripcion}
             />
             <button className="btn btn-primary btn-block" type="submit">
               Agregar
